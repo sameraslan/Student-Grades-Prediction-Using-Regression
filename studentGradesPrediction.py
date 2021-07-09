9347cd9362b84f4ff82ca473030a56aeee9334cb
sameraslan <82460915+sameraslan@users.noreply.github.com>
2021-07-09 12:34:52 -0400

    Update studentGradesPrediction.py

--- studentGradesPrediction.py
+++ studentGradesPrediction.py
@@ -1,18 +1,20 @@
+# This is a very simple linear regression model to predict student grades in math class based on a few features
 import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
 
-# This is a very simple linear regression model to predict student grades in math class based on a few features
 data = pd.read_csv("student-mat.csv", sep=";")
 
-data = data[["G1", "G2", "G3", "study time", "failures", "absences"]]
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
 predict = "G3"
+
 X = np.array(data.drop(["G1", "G2", "G3"], 1))
 y = np.array(data[predict])
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
 
 linear = linear_model.LinearRegression()
 
@@ -20,8 +22,8 @@ linear.fit(x_train, y_train)
 acc = linear.score(x_test, y_test)
 print(acc)
 
-print("Coefficients: ", linear.coef_)
-print("Intercept: ", linear.intercept_)
+print('Coefficient: \n', linear.coef_)
+print('Intercept: \n', linear.intercept_)
 
 predictions = linear.predict(x_test)
 
